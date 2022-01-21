065285587b5c01bbd97a308521a19e9e194fc95a
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-21 11:05:34 -0300

    Criando operação de instanciação

--- backend/src/main/java/com/curso/lojaapp/LojaappApplication.java
+++ backend/src/main/java/com/curso/lojaapp/LojaappApplication.java
@@ -1,13 +1,30 @@
 package com.curso.lojaapp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.curso.lojaapp.domain.Categoria;
+import com.curso.lojaapp.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaappApplication {
+public class LojaappApplication  implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaappApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
