e981d038950f9f1d634e57206694faf1bd53cf3e
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-21 17:31:16 -0300

    Estado e cidade

--- backend/src/main/java/com/curso/lojaapp/LojaappApplication.java
+++ backend/src/main/java/com/curso/lojaapp/LojaappApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.lojaapp.domain.Categoria;
+import com.curso.lojaapp.domain.Cidade;
+import com.curso.lojaapp.domain.Estado;
 import com.curso.lojaapp.domain.Produto;
 import com.curso.lojaapp.repositories.CategoriaRepository;
+import com.curso.lojaapp.repositories.CidadeRepository;
+import com.curso.lojaapp.repositories.EstadoRepository;
 import com.curso.lojaapp.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class LojaappApplication  implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaappApplication.class, args);
@@ -43,5 +53,18 @@ public class LojaappApplication  implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
