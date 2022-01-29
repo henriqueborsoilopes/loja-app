44fb5f9e949ceaa60ca1c168a1ad42aa29381590
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-29 14:13:22 -0300

    Adicionando senha a cliente

--- backend/src/main/java/com/curso/lojaapp/services/DBService.java
+++ backend/src/main/java/com/curso/lojaapp/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.curso.lojaapp.domain.Categoria;
@@ -59,6 +60,9 @@ public class DBService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	public void instantiateTestDatabase() throws ParseException {
 		
@@ -120,7 +124,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	
-		Cliente cli1 = new Cliente(null, "Vitor", "vitorpierrii@gmail.com", "025.184.586-54", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Vitor", "vitorpierrii@gmail.com", "025.184.586-54", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("12345678"));
 		cli1.getTelefones().addAll(Arrays.asList("(45) 9 9916-1342", "(48) 9 8456-5482"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
