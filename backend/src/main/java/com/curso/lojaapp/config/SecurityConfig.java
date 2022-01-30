8b73f9c551b6c1222514c49c7d0dd89388ee7b5a
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-30 11:05:03 -0300

    Implementando autorização

--- backend/src/main/java/com/curso/lojaapp/config/SecurityConfig.java
+++ backend/src/main/java/com/curso/lojaapp/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.curso.lojaapp.security.JWTAuthenticationFilter;
+import com.curso.lojaapp.security.JWTAuthorizationFilter;
 import com.curso.lojaapp.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
