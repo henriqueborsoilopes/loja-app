1cb0aa39046bbebbfca4aee9d6d5c5191bb29f54
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-30 14:48:52 -0300

    Esqueci a senha

---
+++ backend/src/main/java/com/curso/lojaapp/services/AuthService.java
@@ -0,0 +1,59 @@
+package com.curso.lojaapp.services;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.curso.lojaapp.domain.Cliente;
+import com.curso.lojaapp.repositories.ClienteRepository;
+import com.curso.lojaapp.services.exception.ObjectNotFoundException;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private EmailService emailService;
+	
+	private Random random = new Random();
+	
+	public void sendNewPassword(String email) {
+		Cliente cliente = clienteRepository.findByEmail(email);
+		if(cliente == null) {
+			 throw new ObjectNotFoundException("Email não encontrado");
+		}
+		
+		String newPass = newPassword();
+		cliente.setSenha(bCryptPasswordEncoder.encode(newPass));
+		
+		clienteRepository.save(cliente);
+		
+		emailService.sendNewPasswordEmail(cliente, newPass);
+	}
+
+	private String newPassword() {
+		char[] vet = new char[10];
+		for(int i = 0; i < 10; i++) {
+			vet[i] = randomChar();
+		}
+		return new String(vet);
+	}
+
+	private char randomChar() {
+		int opt = random.nextInt();
+		if(opt == 0) {
+			return (char) (random.nextInt(10) + 48);
+		} else if(opt == 1) {
+			return (char) (random.nextInt(26) + 65);
+		} else {
+			return (char) (random.nextInt(26) + 97);
+		}
+	}
+}
